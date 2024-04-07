054b1cf8afc2dbd6c7b70b1938c06458d8c0b2b0
306bobby-android <143777624+306bobby-android@users.noreply.github.com>
2024-04-07 15:45:29 -0400

    Disable artifact paths

--- device-lineage.mk
+++ device-lineage.mk
@@ -8,6 +8,7 @@ $(call inherit-product, device/google/redbull/device-lineage.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-lineage
+DISABLE_ARTIFACT_PATH_REQUIREMENTS += true
 
 # CHRE
 $(call soong_config_set,chre,chre_daemon_dsp_library,//vendor/google/redfin:libadsprpc)
