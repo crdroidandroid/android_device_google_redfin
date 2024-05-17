64b02807711ade78da5de7043f1cca56249d8da4
Bobby <143777624+306bobby-android@users.noreply.github.com>
2024-05-17 08:17:42 -0400

    Revert "Large merge from Lineage"
    This reverts commit 1c30a1aa2c562e50347a240e62ed8cfbe42a618c.

--- device-lineage.mk
+++ device-lineage.mk
@@ -11,6 +11,3 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-lineage
 
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
-
-# wireless_charger HAL service
-include device/google/redfin/wireless_charger/wireless_charger.mk
